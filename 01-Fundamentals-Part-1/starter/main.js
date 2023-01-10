b489985617dcc6b0f1a44813b3dac24ecdb10bfa
Lazaro <lazaroalsina@gmail.com>
2023-01-09 21:53:20 -0500

    finish code challege 2

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -1,12 +1,10 @@
 /*
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
 
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs using the formula (you can even implement both versions)
-3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
 
-TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+HINT: Use an if/else statement 😉
 
 GOOD LUCK 😀
 */
@@ -22,6 +20,14 @@ function bmiCalulator (mass, height) {
 markbmi = bmiCalulator(mark.weight, mark.height);
 johnbmi = bmiCalulator(john.weight, john.height);
 
-let markHigherBMI = markbmi > johnbmi
+let markHigherBMI = markbmi > johnbmi;
 
-console.log(markbmi,johnbmi, markHigherBMI)
+let answer;
+    if (markHigherBMI){
+        answer = 'Mark is fatter'
+    } else {
+        answer = "John is fatter"
+    }
+
+
+console.log(`Mark's BMI is ${markbmi} and John's BMI is ${johnbmi} ${answer}`)
