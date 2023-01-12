01dfcff4acff22d82ce65524ae124932fc452ac3
Lazaro <lazaroalsina@gmail.com>
2023-01-11 21:31:06 -0500

    code challege #4

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -1,102 +1,24 @@
 /*
-The Complete JavaScript Course 7
+Coding Challenge #4
 
-Coding Challenge #3
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
 
-There are two gymnastics teams, Dolphins and Koalas. They compete against each
-other 3 times. The winner with the highest average score wins a trophy!
 Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
 
-1. Calculate the average score for each team, using the test data below
-
-2. Compare the team's average scores to determine the winner of the competition,
-and print it to the console. Don't forget that there can be a draw, so test for that
-as well (draw means they have the same average score)
-
-3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
-team only wins if it has a higher score than the other team, and the same time a
-score of at least 100 points. Hint: Use a logical operator to test for minimum
-score, as well as multiple else-if blocks 😉
-
-4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
-both teams have the same score and both have a score greater or equal 100
-points. Otherwise, no team wins the trophy
 Test data:
+§ Data 1: Test for bill values 275, 40 and 430
 
-§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
-
-§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-
-§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 */
 
-// This obj will keep score of the teams in an array.
-const scoreBoard = {
-    dolphins: [97, 112, 101],
-    koalas: [109, 95, 106]
-};
-
-//These are used for the announctmentFunc to know the names of the teams.
-let team1 = "Dolphins";
-let team2 = "Koalas";
-
-
-//This var will be used to have a min set score in which it can be reassign when needed.
-let minScore = 100
-
-//Avg score function will take an array and interate them to get the avg score.
-function avgScore (scoreInput) {
-    let cal = 0;
-    let i;
-    for (i = 0; i < scoreInput.length; i++) {
-        cal += scoreInput[i];
-    }
-    let avg = cal / i;
-    return avg;
-};
-
-//Winner function will compare two teams if they win and will use a global var called min that is the min score to win or be set as draw
-function winnerFunc (teamA, teamB){
-    let winner = "No winner"
-    if (teamA > teamB){
-        if (teamA > minScore){
-            return winner = "Team A wins";
-        };
-    };
-
-    if (teamB > teamA){
-        if (teamB > minScore){
-            return winner = "Team B wins";
-        };
-    };
-
-    if (teamA === teamB && teamA > minScore && teamB > minScore) {
-        return winner = "Draw!";
-    };
-    return winner;
-};
-
-//announcements is a function that will tell the user who won and will display detail inforamtion about it base on the outcome of the winnerFunc.
-function announcements (winner) {
-    let announcements;
-    let winningTeam = "The match is a Draw!";
-    if(winner === "Team A wins"){
-        winningTeam = `Winner is ${team1}!`;
-    };
-    
-    if(winner === "Team B wins"){
-        winningTeam = `Winner is ${team2}!`;
-    };
-    announcements = `We have a conclusion to the match! The ${winningTeam}`;
-    return announcements;   
-};
-
-let dolphinsScore = avgScore(scoreBoard.dolphins);
-let koalasScore = avgScore(scoreBoard.koalas);
-let winner = winnerFunc(dolphinsScore, koalasScore);
-let boradcastWinner = announcements(winner);
-
-// console.log(dolphinsScore)
-// console.log(koalasScore)
-// console.log(winner)
-console.log(boradcastWinner)
