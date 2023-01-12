2df16cbd603daf8f41777e8edd446daee2cae08a
Lazaro <lazaroalsina@gmail.com>
2023-01-11 21:48:36 -0500

    completed challege 4

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -21,4 +21,9 @@ Hints:
 § To calculate 20% of a value, simply multiply it by 20/100 = 0.2
 § Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 */
+let billTest = [275, 40, 430]
+let bill = billTest[2]
+const tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .20;
 
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value
+${bill + tip}`)
